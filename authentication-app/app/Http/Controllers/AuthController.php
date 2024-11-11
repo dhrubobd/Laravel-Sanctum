d1fcf8becc0c56c35642364679a312e5e44c726d
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-11-12 00:51:24 +0600

    User Registration Function is written and checked with Postman

--- authentication-app/app/Http/Controllers/AuthController.php
+++ authentication-app/app/Http/Controllers/AuthController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function registerUser(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|max:100|unique:users',
+            'password'=>'required|string|min:6|confirmed'
+        ]);
+
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password
+        ]);
+        if($user!=null){
+            return response()->json([
+                'success'=>true,
+                'message'=>"User Created Successfully"
+            ]);
+        }else{
+            return response()->json([
+                'success'=>false,
+                'message'=>"User Created Successfully"
+            ]);
+        }
+    }
 }
